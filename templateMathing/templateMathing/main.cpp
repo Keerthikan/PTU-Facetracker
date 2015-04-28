5bee82d5842bfbf58e5e98d6fa30985e8a5c6a9a
Keerthikan Ratnarajah <kerat12@student.sdu.dk>
2015-04-28 23:59:23 +0200

    Surf

--- templateMathing/templateMathing/main.cpp
+++ templateMathing/templateMathing/main.cpp
@@ -1,98 +1,104 @@
-//
-//  main.cpp
-//  templateMathing
-//
-//  Created by Keerthikan Ratnarajah on 28/04/15.
-//  Copyright (c) 2015 Keerthikan Ratnarajah. All rights reserved.
-//
-
+#include <stdio.h>
 #include <iostream>
-#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
 
-using namespace std;
 using namespace cv;
-Mat ROI;
-Mat ROIS;
+using namespace std;
+void readme();
 
-Point normCrossCorrelation(Mat imgI, Mat ROI/*,vector<Point2f> points1, vector<uchar> status,*/, int method)
+/** @function main */
+int main( int argc, char** argv )
 {
-    Mat res,rec1;
-    Size size(307,230);
-    //for(int i = 0; i <= points1.size(); i++)
-   //{
-        //if(status[i] == 1)
-        //{
-            //getRectSubPix(imgI,size,points1[i],rec1);
-            matchTemplate(imgI, ROI, res, method);
-            //cout << ((float *)(res.operator _IplImage().imageData))[0] << endl;
-        //}
-    //}
-    normalize(res,res,0,1,NORM_MINMAX,-1,cv::Mat());
-    double minVal,maxVal;
-    Point minLoc,maxLoc;
     
-    minMaxLoc(res,&minVal,&maxVal,&minLoc,&maxLoc,cv::Mat());
+    Mat img_object = imread("/Users/keerthikanratnarajah/PTU-Facetracker/templateMathing/templateimg/manRoi.jpg", CV_LOAD_IMAGE_GRAYSCALE );
+    Mat img_scene = imread("/Users/keerthikanratnarajah/PTU-Facetracker/templateMathing/sourceimg/man.jpg", CV_LOAD_IMAGE_GRAYSCALE );
+    if( !img_object.data || !img_scene.data )
+    { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
     
-    cout << "new location: " << maxLoc << " Maxval: " << maxVal << endl;
+    //-- Step 1: Detect the keypoints using SURF Detector
+    int minHessian = 400;
+    SurfFeatureDetector detector( minHessian );
     
-    return minLoc;
-}
-
-Rect Facerecognizer(Mat &now_frame)
-{
-
-    ROI = imread("/Users/keerthikanratnarajah/PTU-Facetracker/templateMathing/sourceimg/man.jpg",CV_LOAD_IMAGE_GRAYSCALE);
-    //cvtColor(now_frame, next, CV_BGR2GRAY);
-    //equalizeHist(next,next);
-    Point center_of_frame(now_frame.size().width/2,now_frame.size().height/2);
-    circle(now_frame, center_of_frame, 1, CV_RGB(0,255,255),8,8,0);
-    String face_cascade_XML = "/Users/keerthikanratnarajah/Downloads/opencv-master/data/haarcascades/haarcascade_frontalface_alt2.xml";
-    CascadeClassifier face_cascade;
-    vector<Rect> faces(1);
-    if( !face_cascade.load( face_cascade_XML ) ){
-        cout << "Cascade Error" << endl;
-    };
-    face_cascade.detectMultiScale( now_frame, faces, 1.1, 2, CASCADE_DO_CANNY_PRUNING, Size(300, 300) );
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
     
-    if (!faces.empty()) {
-        for( auto faces : faces)
-        {
-            Point center_position_of_face((faces.br().x+faces.tl().x)/2,(faces.tl().y+faces.br().y)/2);
-            Point corner_1(faces.br().x,faces.tl().y);
-            Point corner_2 = faces.tl();
-            Point corner_3 = faces.br();
-            Point corner_4(faces.tl().x,faces.br().y);
-            rectangle(now_frame, faces, CV_RGB(0,255,0),4,8,0);
-            circle(now_frame, center_position_of_face, 8, CV_RGB(128,128,128),8,8,0);
-            circle(now_frame, corner_1, 1, CV_RGB(128,128,128),8,8,0);
-            circle(now_frame, corner_2, 1, CV_RGB(128,128,128),8,8,0);
-            circle(now_frame, corner_3, 1, CV_RGB(128,128,128),8,8,0);
-            circle(now_frame, corner_4, 1, CV_RGB(128,128,128),8,8,0);
-            line(now_frame, center_position_of_face, center_of_frame, CV_RGB(0,200,0),1,8);
-            
-        }
-        
+    detector.detect( img_object, keypoints_object );
+    detector.detect( img_scene, keypoints_scene );
+    
+    //-- Step 2: Calculate descriptors (feature vectors)
+    SurfDescriptorExtractor extractor;
+    
+    Mat descriptors_object, descriptors_scene;
+    
+    extractor.compute( img_object, keypoints_object, descriptors_object );
+    extractor.compute( img_scene, keypoints_scene, descriptors_scene );
+    //-- Step 3: Matching descriptor vectors using FLANN matcher
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
+    matcher.match( descriptors_object, descriptors_scene, matches );
+    
+    double max_dist = 0; double min_dist = 100;
+    
+    //-- Quick calculation of max and min distances between keypoints
+    for( int i = 0; i < descriptors_object.rows; i++ )
+    { double dist = matches[i].distance;
+        if( dist < min_dist ) min_dist = dist;
+        if( dist > max_dist ) max_dist = dist;
     }
-    //flip(now_frame, now_frame, 1);
-    imshow( "Facerecognizer", now_frame );
-    ROIS = ROI(faces.back());
-    return  faces.back();
-}
-
-int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
-    Mat frame;
-    frame = imread("/Users/keerthikanratnarajah/PTU-Facetracker/templateMathing/sourceimg/man.jpg",CV_LOAD_IMAGE_GRAYSCALE);
-    Rect face;
-    face = Facerecognizer(frame);
-    destroyAllWindows();
-    Mat Source = imread("/Users/keerthikanratnarajah/PTU-Facetracker/templateMathing/sourceimg/man.jpg",CV_LOAD_IMAGE_GRAYSCALE);
-    imwrite("/Users/keerthikanratnarajah/PTU-Facetracker/templateMathing/templateimg/manRoi.jpg", ROIS);
-    Point max = normCrossCorrelation(frame,ROI,CV_TM_CCOEFF_NORMED);
-    circle(Source, max, 11, Scalar(0,0,255),8,2);
-    imshow("template", ROIS);
-    imshow("source", Source);
-    waitKey();
     
+    printf("-- Max dist : %f \n", max_dist );
+    printf("-- Min dist : %f \n", min_dist );
+    
+    //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+    std::vector< DMatch > good_matches;
+    
+    for( int i = 0; i < descriptors_object.rows; i++ )
+    { if( matches[i].distance < 3*min_dist )
+    { good_matches.push_back( matches[i]); }
+    }
+    
+    Mat img_matches;
+    drawMatches( img_object, keypoints_object, img_scene, keypoints_scene,
+                good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+    
+    //-- Localize the object
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+    
+    for( int i = 0; i < good_matches.size(); i++ )
+    {
+        //-- Get the keypoints from the good matches
+        obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+        scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+    }
+    
+    Mat H = findHomography( obj, scene, CV_RANSAC );
+    
+    //-- Get the corners from the image_1 ( the object to be "detected" )
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
+    obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
+    std::vector<Point2f> scene_corners(4);
+    
+    perspectiveTransform( obj_corners, scene_corners, H);
+    
+    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+    line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
+    line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+    
+    //-- Show detected matches
+    imshow( "Good Matches & Object detection", img_matches );
+    
+    waitKey(0);
     return 0;
 }
+
+/** @function readme */
+void readme()
+{ std::cout << " Usage: ./SURF_descriptor <img1> <img2>" << std::endl; }
