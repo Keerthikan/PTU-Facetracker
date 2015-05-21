ea5ba21e5f7fc201716f90b61799e80a458f4717
Keerthikan Ratnarajah <kerat12@student.sdu.dk>
2015-05-21 10:14:12 +0200

    Use of Matlab Script since mathmatica is fucked

---
+++ SystemIdent/Ident/Matlab_script.m
@@ -0,0 +1,12 @@
+ %% Time specifications:
+   Fs = 1;                   % samples per second
+   dt = 1/Fs;                   % seconds per sample
+   StopTime = 500;             % seconds
+   t = (0:dt:StopTime-dt)';     % seconds
+   Fc = 1/100;                     % hertz
+   x = 0.799760*sin(2*pi*Fc*t);
+   % Plot the signal versus time:
+   figure;
+   plot(t,x);
+   xlabel('time (in seconds)');
+   title('Signal versus Time');
