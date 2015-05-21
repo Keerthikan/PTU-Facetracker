965b98937e29e191af7330360db54b66eeded24d
Keerthikan Ratnarajah <kerat12@student.sdu.dk>
2015-05-21 11:31:44 +0200

    f

--- SystemIdent/Ident/Matlab_script.m
+++ SystemIdent/Ident/Matlab_script.m
@@ -1,14 +1,14 @@
  %% Time specifications:
-   Fs = 10;                   % samples per second
+   Fs = 100;                   % samples per second
    dt = 1/Fs;                   % seconds per sample
-   StopTime = 500;             % seconds
+   StopTime = 100;             % seconds
    t = (0:dt:StopTime-dt)';     % seconds
    Fc = 1;                     % hertz
-   x = chop(0.799760*sin(2*pi*Fc/Fs*t),5); %plot sine wave and chop the input to 5 sign. decimal
+   x = chop(0.799760*sin(2*pi*Fc*t),5); %plot sine wave and chop the input to 5 sign. decimal
    x = transpose(x);    % transposin => becomes a vector
    % Plot the signal versus time:
    figure;
    plot(t,x);
    %xlabel('time (in seconds)');
    %title('Signal versus Time');
-   csvwrite('onetenth_sampl.csv',x) % save the data somewhere on github. 
+   csvwrite('onehertz_sample100.csv',x) % save the data somewhere on github. 
