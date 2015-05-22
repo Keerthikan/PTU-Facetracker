957beae3f04bd81b5a6e83be9818ce6bf7ded0b5
Keerthikan Ratnarajah <kerat12@student.sdu.dk>
2015-05-22 11:55:27 +0200

    New data set

--- SystemIdent/Ident/Matlabsqript_with_chirp.m
+++ SystemIdent/Ident/Matlabsqript_with_chirp.m
@@ -9,7 +9,7 @@ plot(t, y, '*-');
 %%
 fs = 30; % sample rate
 f1 = 1/10; % start freq
-f2 = 10; % end freq
+f2 = 5; % end freq
 T = 100; %second run time 
  
 w1=2*pi*f1;
@@ -18,5 +18,5 @@ t=0:1/fs:(T*fs-1)/fs;
 K=T*w1/log(w2/w1);
 L=T/log(w2/w1);
 sweep=0.799760*sin(K.*(exp(t./L)-1)); %sweep data set
-csvwrite('newFreqSweep100S30SAMPLEFREQ_ONETEENTHtoTEN.csv',sweep);
+csvwrite('newFreqSweep100S30SAMPLEFREQ_ONETEENTHtoFIVE.csv',sweep);
 plot(t,sweep,'*-')
