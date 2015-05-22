25044d54e47fe5965e15bd3cde66bea602019ca1
Keerthikan Ratnarajah <kerat12@student.sdu.dk>
2015-05-22 13:19:03 +0200

    New dataset oneteenth to on

--- SystemIdent/Ident/Matlabsqript_with_chirp.m
+++ SystemIdent/Ident/Matlabsqript_with_chirp.m
@@ -7,9 +7,9 @@ t = 0:1/fs:100;
 y = 0.79*chirp(t, a, t(end), b,'logarithmic');
 plot(t, y, '*-');
 %%
-fs = 30; % sample rate
+fs = 10; % sample rate
 f1 = 1/10; % start freq
-f2 = 2; % end freq
+f2 = 1; % end freq
 T = 100; %second run time 
  
 w1=2*pi*f1;
@@ -17,6 +17,6 @@ w2=2*pi*f2;
 t=0:1/fs:(T*fs-1)/fs;
 K=T*w1/log(w2/w1);
 L=T/log(w2/w1);
-sweep=0.349880*sin(K.*(exp(t./L)-1)); %sweep data set
-csvwrite('newFreqSweep100S30SAMPLEFREQ_ONETEENTHtoTWO.csv',sweep);
+sweep=0.799760*sin(K.*(exp(t./L)-1)); %sweep data set
+csvwrite('newFreqSweep100S10SAMPLEFREQ_ONETEENTHtoONE.csv',sweep);
 plot(t,sweep,'*-')
