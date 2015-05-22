242db2a5742b21c638baffff0e3e61b201c4f769
Keerthikan Ratnarajah <kerat12@student.sdu.dk>
2015-05-22 11:50:46 +0200

    orden samt nyt data sæt

--- SystemIdent/Ident/Matlabsqript_with_chirp.m
+++ SystemIdent/Ident/Matlabsqript_with_chirp.m
@@ -7,9 +7,9 @@ t = 0:1/fs:100;
 y = 0.79*chirp(t, a, t(end), b,'logarithmic');
 plot(t, y, '*-');
 %%
-fs = 10; % sample rate
+fs = 30; % sample rate
 f1 = 1/10; % start freq
-f2 = 2; % end freq
+f2 = 10; % end freq
 T = 100; %second run time 
  
 w1=2*pi*f1;
@@ -18,5 +18,5 @@ t=0:1/fs:(T*fs-1)/fs;
 K=T*w1/log(w2/w1);
 L=T/log(w2/w1);
 sweep=0.799760*sin(K.*(exp(t./L)-1)); %sweep data set
-csvwrite('newFreqSweep100S10SAMPLEFREQ_ONETEENTHtoTWO.csv',sweep);
+csvwrite('newFreqSweep100S30SAMPLEFREQ_ONETEENTHtoTEN.csv',sweep);
 plot(t,sweep,'*-')
